933f9e5eb9020689800523edf117a7663984d79a
emmaindal <emma.indahl@gmail.com>
2019-04-08 14:52:35 +0200

    created the website structure

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Enhörningar</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" media="screen" href="style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body class="container">
+  <h1>Enhörningar</h1>
+  <hr>
+  <form action="/" method="post">
+    <label for="unicorn-id">Skriv in enhörnings id: </label>
+    <input type="text" id="unicorn-id" name="unicorn-id" class="form-control">
+    <button type="submit" class="btn btn-success">Visa enhörning</button>
+    <button type="submit" class="btn btn-dark">Visa alla enhörningar</button>
+  </form>
+  <section>
+
+  </section>
+</body>
+
+</html>
