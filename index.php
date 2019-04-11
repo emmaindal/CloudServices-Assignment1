f725e727ecc9a5d20a204b99eec76f73402690ce
emmaindal <emma.indahl@gmail.com>
2019-04-11 21:28:20 +0200

    fetching all unicorns and a spcific unicorn from unicorn API

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
+<?php
+
+require 'vendor/autoload.php';
+$client = new \GuzzleHttp\Client();
+
+function getUnicorn($id, $client){
+  $response = $client->request('GET', "http://unicorns.idioti.se/$id", ['headers' => [
+    'Accept' => 'application/json']])->getBody();
+  $unicorn = json_decode($response, true);
+  return $unicorn;
+}
+
+function getUnicorns($client){
+  $response = $client->request('GET', "http://unicorns.idioti.se/", ['headers' => [ 'Accept' => 'application/json']])->getBody();
+  $unicorns = json_decode($response, true);
+  return $unicorns;
+}
+
+$unicornId = $_GET["id"];
+if ($unicornId) {
+  $unicorn = getUnicorn($unicornId, $client);
+} else {
+  $unicorns = getUnicorns($client);
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -14,15 +41,62 @@
 <body class="container">
   <h1>Enhörningar</h1>
   <hr>
-  <form action="/" method="post">
+  <form action="/" method="get">
     <label for="unicorn-id">Skriv in enhörnings id: </label>
-    <input type="text" id="unicorn-id" name="unicorn-id" class="form-control">
-    <button type="submit" class="btn btn-success">Visa enhörning</button>
-    <button type="submit" class="btn btn-dark">Visa alla enhörningar</button>
+    <input type="text" id="unicorn-id" name="id" class="form-control">
+    <button type="submit" class="btn btn-primary">Visa enhörning</button>
+    <button type="submit" class="btn btn-outline-primary"><a class="link" href="/">Visa alla enhörningar</a></button>
   </form>
   <section>
-
-  </section>
+    <?php
+        if ($unicorn) {
+          $name = $unicorn["name"];
+          $description = $unicorn["description"];
+          $dateTime = $unicorn["spottedWhen"];
+          $date = substr($dateTime, 0, 10);
+          $image = $unicorn["image"];
+          $reportedBy = $unicorn["reportedBy"];
+          echo "<div class=\"card mb-3\" style=\"max-width: 100%\">
+                  <div class=\"row no-gutters\">
+                    <div class=\"col-md-4\">
+                      <img src=\"$image\" class=\"card-img\" alt=\"...\">
+                    </div>
+                    <div class=\"col-md-8\">
+                      <div class=\"card-body\">
+                        <h5 class=\"card-title\">$name</h5>
+                        <small class=\"text-muted\">$date</small>
+                        <p class=\"card-text\">$description</p>
+                        <p class=\"card-text\"><small class=\"text-muted\">Rapporterad av: $reportedBy</small></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>";
+        } else {
+        echo "<h5>Alla Enhörningar</h5>";
+        $i = 0;
+        foreach($unicorns as $unicorn){
+          $i++;
+          $name = $unicorn["name"];
+          if (!$name == "") {
+            $id = $unicorn["id"];
+            $details = $unicorn["details"];
+            echo "<div class=\"card mb-3\" style=\"max-width: 100%\">
+                  <div class=\"row no-gutters\">
+                    <div class=\"col-md-12\">
+                        <div class=\"card-body unicorn-card\">
+                            <h5 class=\"card-title\">$i: $name</h5>
+                            <button class=\"btn btn-outline-dark\"><a class=\"link\" href=\"/?id=$id\">Läs mer</a></button>
+                    
+                      </div>
+                    </div>
+                  </div>
+                </div>";
+            }
+          }
+        }
+        
+      ?>
+    </sectio>
 </body>
 
 </html>
